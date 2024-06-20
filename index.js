20a7fddaf2aeace26b3dd909ed38fc2ef113e609
Mohamed Raslen Safsaf <raslen.safsaf@firstdeliverygroup.com>
2024-06-20 10:33:40 +0100

    adding variable file in docker-compose

--- index.js
+++ index.js
@@ -95,4 +95,4 @@ app.use((req, res, next) => {
     next();
 });
 
-app.listen(process.env.PORT, () => console.log('Server running on port 5500'));
+app.listen(process.env.PORT, () => console.log(`Server running on port${process.env.PORT}`));
