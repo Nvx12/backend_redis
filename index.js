7d2bb38fe6542d3f1c28d6e23e2656d5b271f1d7
Mohamed Raslen Safsaf <raslen.safsaf@firstdeliverygroup.com>
2024-06-20 10:29:07 +0100

    Fixing caching in create method

--- index.js
+++ index.js
@@ -36,6 +36,22 @@ const http_request_total = new client.Counter({
         metric_label_enum.STATUS_CODE,
     ],
 });
+const httpRequestDurationMicroseconds = new Prometheus.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['route'],
+    // buckets for response time from 0.1ms to 500ms
+    buckets: [0.10, 5, 15, 50, 100, 200, 300, 400, 500]
+});
+app.use((req, res, next) => {
+    const responseTimeInMs = Date.now() - res.locals.startEpoch
+
+    httpRequestDurationMicroseconds
+        .labels(req.method, req.route.path, res.statusCode)
+        .observe(responseTimeInMs)
+
+    next()
+})
 client.collectDefaultMetrics({ register })
 register.registerMetric(http_request_total);
 
